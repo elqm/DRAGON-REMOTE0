5a92b3155dc6e90f0802bc0c895a5c39d1bf38fd
pyunghun cho <pyunghun120@gmail.com>
2023-02-20 18:27:32 +0900

    [feature/ user] login
    로그인 화면 크기 , 이미지 수정

--- src/com/greedy/user/Application.java
+++ src/com/greedy/user/Application.java
@@ -5,8 +5,8 @@ public class Application {
 	public static void main(String[] args) {
 
 		
-//		new GameStartPage().display();
-//		new GameLoginPage().display2();
+    	new GameStartPage().display();
+		new GameLoginPage().display2();
 		new GameUserLoginPage().display3();
 	}
 
