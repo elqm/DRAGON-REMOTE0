a7426730ebbf616a29bc6b5f52d1abf45d786f25
leebumsung <leebumsung3@naver.com>
2023-02-22 19:03:14 +0900

    easyGame
    피격추가 score 추가 미완성

---
+++ src/com/greedy/inGame/easy/display1.java
@@ -0,0 +1,69 @@
+package com.greedy.inGame.easy;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+
+public class display1 {
+
+	private int x, y;
+    private Timer timer;
+    
+	public void displayInfo() {
+		
+		JFrame mF = new JFrame();
+		
+		mF.setBounds(300, 200, 1000, 700);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		
+		Image image = new ImageIcon("images/easyGameInfo.png").getImage().getScaledInstance(800, 550, 0);
+		
+		JLabel label = new JLabel(new ImageIcon(image));
+		
+//		JButton button1 = new JButton("게임 시작");
+		JButton button1 = new JButton(new ImageIcon("images/playButton.png"));
+		button1.addActionListener(new ActionListener() {
+	        @Override
+	        public void actionPerformed(ActionEvent e) {
+//	        mF.setVisible(false);
+	        mF.remove(panel);
+	        mF.add(new TestMoveCharacter());
+	        mF.repaint();
+	        mF.revalidate();
+	        mF.setSize(1400, 800);
+	        }
+	    });
+		
+		label.setBounds(10, 10, 900, 600);
+		button1.setBounds(580, 480, 140, 35);
+		
+		panel.add(button1);
+		panel.add(label);
+		
+		mF.add(panel);
+		
+		mF.setVisible(true);
+		mF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+}
+
+
+
+
+
+
+
---
+++ src/com/greedy/inGame/easy/easyGame1.java
@@ -0,0 +1,99 @@
+package com.greedy.inGame.easy;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class easyGame1 extends JPanel implements ActionListener {
+
+    private Image backgroundImage;
+    private Image characterImage;
+    private Image weaponImage;
+    private int characterX;
+    private int characterY;
+    private int weaponX;
+    private int weaponY;
+    private int score;
+
+    public easyGame1() {
+        ImageIcon bgImage = new ImageIcon("images/easyBackground1.png");
+        backgroundImage = bgImage.getImage();
+        ImageIcon charImage = new ImageIcon("images/dragon1.png");
+        characterImage = charImage.getImage();
+        ImageIcon weapImage = new ImageIcon("images/easyarrow.png");
+        weaponImage = weapImage.getImage();
+        characterX = 50;
+        characterY = 250;
+        weaponX = 0;
+        weaponY = 0;
+        score = 10;
+
+        setPreferredSize(new Dimension(800, 600));
+
+        // Set up key listener
+        setFocusable(true);
+        addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                int keyCode = e.getKeyCode();
+                if (keyCode == KeyEvent.VK_LEFT) {
+                    characterX -= 15;
+                } else if (keyCode == KeyEvent.VK_RIGHT) {
+                    characterX += 15;
+                } else if (keyCode == KeyEvent.VK_UP) {
+                    characterY -= 15;
+                } else if (keyCode == KeyEvent.VK_DOWN) {
+                    characterY += 15;
+                }
+            }
+        });
+
+        // Set up timer to generate falling weapon images
+        Timer timer = new Timer(2000, this);
+        timer.start();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, null);
+        g.drawImage(characterImage, characterX, characterY, null);
+        g.drawImage(weaponImage, weaponX, weaponY, null);
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.BOLD, 16));
+        g.drawString("Score: " + score, 10, 20);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        Random rand = new Random();
+        weaponX = rand.nextInt(700);
+        weaponY = 0;
+        while (weaponY < 500) {
+            weaponY += 5;
+            repaint();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException ex) {}
+            if (weaponX > characterX && weaponX < characterX + 50 && weaponY > characterY && weaponY < characterY + 50) {
+                score--;
+                weaponX = 0;
+                weaponY = 0;
+            }
+        }
+        if (weaponY >= 500) {
+            weaponX = 0;
+            weaponY = 0;
+        }
+        if (score == 0) {
+            JOptionPane.showMessageDialog(null, "Game Over");
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("easyGame1");
+        easyGame1 easygame = new easyGame1();
+        frame.add(easygame);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
