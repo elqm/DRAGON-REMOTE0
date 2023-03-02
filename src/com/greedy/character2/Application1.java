f2941b3dd7b7b8652f831d4db3e0deae27b460a6
pyunghun cho <pyunghun120@gmail.com>
2023-03-02 09:19:15 +0900

    [feature user10] user 화

---
+++ src/com/greedy/character2/Application1.java
@@ -0,0 +1,20 @@
+
+package com.greedy.character2;
+
+import javax.swing.JPanel;
+
+public class Application1 {
+
+	public static void main(String[] args) {
+		
+//		new choiceCharacter1().display();
+//		new choiceCharacter21().display();
+//		new choiceGlanun1().Glanun();
+//		new choiceHyton1().Hyton();
+//		new choiceZorg1().Zorg();
+//		new gradeGlanun1().gradeGlanun();
+//		new gradeHyton1().gradeHyton();
+		new gradeZorg1().gradeZorg();
+//		
+	}
+}
